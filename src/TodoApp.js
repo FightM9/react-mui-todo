e773964e20a38cca05e8399b1dd10f7721dcd168
FightM9 <fightm9@gmail.com>
2022-03-17 00:22:50 +0300

    master

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
-import Form from "./components/Form";
+import React, { useReducer, useState } from "react";
+import AddTodoForm from "./components/AddTodoForm";
 import { openModalInput } from "./components/modal";
 import TodoList from "./components/TodoList";
 import FakeData from "./utils/fakeData";
 import TodoTabs from "./components/TodoTabs";
 import { Context } from "./utils/context";
+import reducer, { actionType } from "./utils/reduser";
 
 export default function TodoApp() {
-  const [todos, setTodos] = useState(FakeData);
-  const [tab, setTab] = useState(1);
-
-  const createNewTodo = (title) => {
-    return {
-      id: Math.random().toString(36).substring(2, 6),
-      title: title,
-      complete: false,
-    };
-  };
+  const [todos, dispatch] = useReducer(reducer, FakeData);
+  const [tab, setTab] = useState(0);
 
   const getActiveTodoList = function () {
     switch (tab) {
@@ -28,44 +21,24 @@ export default function TodoApp() {
         return todos;
     }
   };
-  const getTodoById = (id) => todos.find((todo) => todo.id === id);
 
-  const getTodoIndexById = (id) => todos.findIndex((todo) => todo.id === id);
 
-  const addTodo = (title) => setTodos([createNewTodo(title), ...todos]);
-
-  const editTodo = (id) => {
-    const oldTitle = getTodoById(id).title;
-    const newTitle = openModalInput(oldTitle);
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, title: newTitle } : { ...todo }
-      )
-    );
-  };
-
-  const removeTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
-      )
-    );
-    console.log(todos);
-  };
+  // const editTodo = (id) => {
+  //   const oldTitle = getTodoById(id).title;
+  //   const newTitle = openModalInput(oldTitle);
+  //   setTodos(
+  //     todos.map((todo) =>
+  //       todo.id === id ? { ...todo, title: newTitle } : { ...todo }
+  //     )
+  //   );
+  // };
 
   return (
-    <Context.Provider value={{editTodo, removeTodo, toggleTodo}}>
+    <Context.Provider value={{ dispatch }}>
       <div>
-        <Form addTodo={addTodo} />
+        <AddTodoForm/>
         <TodoTabs value={tab} setValue={setTab} />
-        <TodoList
-          // Object with functions for update TodoApp
-          // Todo={{ editTodo, removeTodo, toggleTodo }}
-          // Array of TodoApp item
-          todos={getActiveTodoList()}
-        />
+        <TodoList todos={getActiveTodoList()} />
       </div>
     </Context.Provider>
   );
