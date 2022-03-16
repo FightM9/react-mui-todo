e8ac224c3ba817ce71449c0925c26f1b2ebe24cf
FightM9 <fightm9@gmail.com>
2022-03-17 00:23:38 +0300

    Formating text

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,19 +1,28 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../utils/context";
+import { actionType } from "../utils/reduser";
 
-export default function TodoItem({id, title, complete}) {
-  const {editTodo, removeTodo, toggleTodo} = useContext(Context);
+export default function TodoItem({ id, title, complete }) {
+  const { dispatch } = useContext(Context);
 
   return (
     <li>
       <label>
-        <input onChange={() => toggleTodo(id)} type="checkbox" checked={complete}/>
+        <input
+          type="checkbox"
+          checked={complete}
+          onChange={() => dispatch({ type: actionType.TOGGLE, payload: id })}
+        />
         <span></span>
-        {title} 
+        {title}
       </label>
       <span>
-        <button onClick={() => editTodo(id)}>R</button>
-        <button onClick={() => removeTodo(id)}>D</button>
+        <button onClick={() => console.log("EDIT")}>R</button>
+        <button
+          onClick={() => dispatch({ type: actionType.REMOVE, payload: id })}
+        >
+          D
+        </button>
       </span>
     </li>
   );
