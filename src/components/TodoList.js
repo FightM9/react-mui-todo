f7dcfdda59ceb36726e01788ebb92a04e5814113
FightM9 <fightm9@gmail.com>
2022-03-17 22:10:49 +0300

    Fix

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,94 @@
-import React from "react";
-import TodoItem from "./TodoListItem";
+import React, { useContext, useState } from "react";
+import { Context } from "../utils/context";
+import { actionType } from "../utils/reduser";
 
-export default function TodoList({ Todo, todos }) {
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import { Box, IconButton, Typography } from "@mui/material";
+
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import ModeEditOutlinedIcon from "@mui/icons-material/ModeEditOutlined";
+import RenameModal from "./RenameModal";
+
+export default function TodoList({ todos }) {
+  const { dispatch } = useContext(Context);
+  const [openModal, setOpenModal] = useState(false);
+  const [editableTodo, setEditableTodo] = useState({});
+
+  const onEdit = (todo) => {
+    setEditableTodo(todo);
+    if (!editableTodo) return;
+    setOpenModal(true);
+  };
+
+  const onRename = (todo) => {
+    dispatch({
+      type: actionType.EDIT,
+      payload: todo.id,
+      title: todo.title,
+    });
+  };
+
+  const onRemove = (todo) => {
+    dispatch({
+      type: actionType.REMOVE,
+      payload: todo.id,
+    });
+  };
+  
+
+  const onToggleCompletion = (todo) => {
+    dispatch({
+      type: actionType.TOGGLE,
+      payload: todo.id,
+    });
+  };
+
+  if (todos.length == 0) {
+    return (
+      <Box sx={{ p: '36px', width: '100%', textAlign: 'center'}}>   
+         <Typography variant="subtitle2" gutterBottom component="div">
+         There are no tasks to show for now
+         </Typography>
+      </Box>
+    )
+  }
+
   return (
-    <ul>
-      {todos &&
-        todos.map((todo) => (
-          <TodoItem
-            // Todo={Todo}
+    <>
+      <List>
+        {todos.map((todo) => (
+          <ListItem
             key={todo.id}
-            id={todo.id}
-            title={todo.title}
-            complete={todo.complete}
-          />
+            divider
+            disablePadding
+            secondaryAction={
+              <div>
+                <IconButton size="small" onClick={() => onEdit(todo)}>
+                  <ModeEditOutlinedIcon fontSize="small" />
+                </IconButton>
+                <IconButton size="small" onClick={() => onRemove(todo)}>
+                  <DeleteOutlineOutlinedIcon fontSize="small" />
+                </IconButton>
+              </div>
+            }
+          >
+            <ListItemButton sx={{ mr: "30px"}} onClick={() => onToggleCompletion(todo)}>
+              <Checkbox edge="start" checked={todo.complete} disableRipple />
+              <ListItemText sx={{ wordBreak: "break-all" }}  primary={todo.title} />
+            </ListItemButton>
+          </ListItem>
         ))}
-    </ul>
+      </List>
+      <RenameModal
+        editableTodo={editableTodo}
+        onRename={onRename}
+        open={openModal}
+        setOpen={setOpenModal}
+      />
+    </>
   );
 }
