38a508d12790a65d2f6f6c6716bdec37c395f053
FightM9 <fightm9@gmail.com>
2022-03-17 22:11:52 +0300

    Create panel like AppBar

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,20 +1,22 @@
-import { Button } from "@mui/material";
+import { Button, IconButton, InputBase } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Paper from "@mui/material/Paper";
 import React, { useContext, useState } from "react";
 import { Context } from "../utils/context";
 import { actionType } from "../utils/reduser";
 
 export default function AddTodoForm() {
-  const [userInput, setUserInput] = useState('');
-  const { dispatch } = useContext(Context)
+  const [userInput, setUserInput] = useState("");
+  const { dispatch } = useContext(Context);
 
   function onFormSubmit(evt) {
     evt.preventDefault();
-    if (!userInput.trim()) return
+    if (!userInput.trim()) return;
     dispatch({
       type: actionType.ADD,
       payload: userInput,
     });
-    setUserInput('')
+    setUserInput("");
   }
 
   function onChangeInput(evt) {
@@ -22,14 +24,21 @@ export default function AddTodoForm() {
   }
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <input 
-      onChange={onChangeInput} 
-      value={userInput}
-      type="text" 
-      placeholder="Add new" 
+    <Paper
+      component="form"
+      onSubmit={onFormSubmit}
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        onChange={onChangeInput}
+        value={userInput}
+        type="text"
+        placeholder="Add new task"
       />
-      <Button type="submit" variant="contained">Send</Button>
-    </form>
+      <IconButton disabled={!userInput.trim()} sx={{ p: "10px" }} type="submit" aria-label="Add new Todo">
+        <AddIcon />
+      </IconButton>
+    </Paper>
   );
 }
