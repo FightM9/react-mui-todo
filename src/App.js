9a9b7858821ef4f915d40cbb8e84732831238859
FightM9 <fightm9@gmail.com>
2022-03-18 12:23:55 +0300

    Add fliud container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoApp from "./TodoApp";
-import "./App.css"
-
+import { Container } from "@mui/material";
+import "./App.css";
 
 export default function App() {
   return (
-    <TodoApp/>   
+    <Container maxWidth="md">
+      <TodoApp />
+    </Container>
   );
 }
