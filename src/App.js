ebf9206e2333d80d1b28f04c01a70640d65ca4b0
FightM9 <fightm9@gmail.com>
2022-03-19 13:30:36 +0300

    Add Theme Provider for change app theme mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-import TodoApp from "./TodoApp";
+import React, { useState } from "react";
+import TodoApp from "./components/Todo/TodoApp";
+import Header from "./components/Header/Header";
+import theme from "./utils/theme";
 import { Container } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/system";
 import "./App.css";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(true); 
+
   return (
-    <Container maxWidth="md">
-      <TodoApp />
-    </Container>
+    <ThemeProvider theme={darkMode ? theme.dark : theme.light}>    
+      <Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
+        <Header theme={{setDarkMode, darkMode}}/>
+        <Container maxWidth="md">
+          <TodoApp />
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
