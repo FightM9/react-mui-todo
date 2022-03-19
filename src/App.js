b8f9d40b536b7af7a7a979cd8577ee0fbc1c63ec
FightM9 <fightm9@gmail.com>
2022-03-19 18:49:18 +0300

    chore: Include base style in css

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import TodoApp from "./components/Todo/TodoApp";
 import theme from "./utils/theme";
-import { Container } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { Box } from "@mui/system";
-import "./App.css";
 
 export default function App() {
   // Dark mode boolean status
   const [darkMode, setDarkMode] = useState(true); 
 
   return (
-    <ThemeProvider theme={darkMode ? theme.dark : theme.light}>    
-      <Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
+    <ThemeProvider theme={darkMode ? theme.dark : theme.light}>  
+    <CssBaseline/> 
+      <Box sx={{ minHeight: "100vh", minWidth: "475px", bgcolor: "background.default" }}>
         <Header theme={{setDarkMode, darkMode}}/>
         <Container maxWidth="md">
           <TodoApp />
